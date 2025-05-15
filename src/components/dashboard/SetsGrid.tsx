019f40e0e104fac515559b5614f8b54e307b08c4
diebrudie <63471787+diebrudie@users.noreply.github.com>
2025-05-15 11:16:35 +0200

    Update SetsGrid.tsx to a previous version

--- src/components/dashboard/SetsGrid.tsx
+++ src/components/dashboard/SetsGrid.tsx
@@ -1,68 +1,66 @@
-
 import SetBox from "@/components/match/SetBox";
 import { MatchScore } from "@/hooks/use-club-data";
 
-interface SetsGridProps {
+interface SetsGridProps {interface SetsGridProps {
   scores: MatchScore[];
   onScoreUpdate: (setNumber: number, teamAScore: number | null, teamBScore: number | null) => void;
 }
 
-const SetsGrid = ({ scores, onScoreUpdate }: SetsGridProps) => {
-  // Function to find the score for a specific set
-  const getSetScore = (setNumber: number) => {
-    return scores.find(score => score.setNumber === setNumber);
-  };
-
+const SetsGrid = ({ scores, onScoreUpdate }: SetsGridProps) => {const SetsGrid = ({ scores, onScoreUpdate }: SetsGridProps) => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
       {/* Set 1 - Larger box on the left spanning two rows */}
-      <div className="md:col-span-1 md:row-span-2">
+      <div className="md:row-span-2">
         <SetBox
           key={1}
           setNumber={1}
-          teamAScore={getSetScore(1)?.teamAScore}
-          teamBScore={getSetScore(1)?.teamBScore}
+          teamAScore={scores.find(score => score.gameNumber === 1)?.teamA}
+          teamBScore={scores.find(score => score.gameNumber === 1)?.teamB}
           onScoreUpdate={onScoreUpdate}
           isLarge={true}
         />
       </div>
 
-      {/* Top right section: Sets 2 and 4 */}
+      {/* Column 2 top row: Set 2 */}
       <div>
         <SetBox
           key={2}
           setNumber={2}
-          teamAScore={getSetScore(2)?.teamAScore}
-          teamBScore={getSetScore(2)?.teamBScore}
-          onScoreUpdate={onScoreUpdate}
-        />
-      </div>
-      <div>
-        <SetBox
-          key={4}
-          setNumber={4}
-          teamAScore={getSetScore(4)?.teamAScore}
-          teamBScore={getSetScore(4)?.teamBScore}
+          teamAScore={scores.find(score => score.gameNumber === 2)?.teamA}
+          teamBScore={scores.find(score => score.gameNumber === 2)?.teamB}
           onScoreUpdate={onScoreUpdate}
         />
       </div>
 
-      {/* Bottom right section: Sets 3 and 5 */}
+      {/* Column 3 top row: Set 4 */}
+      <div>
+        <SetBox
+          key={4}
+          setNumber={4}
+          teamAScore={scores.find(score => score.gameNumber === 4)?.teamA}
+          teamBScore={scores.find(score => score.gameNumber === 4)?.teamB}
+          onScoreUpdate={onScoreUpdate}
+        />
+      </div>
+
+      {/* Column 2 bottom row: Set 3 */}
       <div>
         <SetBox
           key={3}
           setNumber={3}
-          teamAScore={getSetScore(3)?.teamAScore}
-          teamBScore={getSetScore(3)?.teamBScore}
+          teamAScore={scores.find(score => score.gameNumber === 3)?.teamA}
+          teamBScore={scores.find(score => score.gameNumber === 3)?.teamB}
           onScoreUpdate={onScoreUpdate}
         />
       </div>
+
+      {/* Column 3 bottom row: Set 5 */}
       <div>
         <SetBox
           key={5}
           setNumber={5}
-          teamAScore={getSetScore(5)?.teamAScore}
-          teamBScore={getSetScore(5)?.teamBScore}
+          teamAScore={scores.find(score => score.gameNumber === 5)?.teamA}
+          teamBScore={scores.find(score => score.gameNumber === 5)?.teamB}
           onScoreUpdate={onScoreUpdate}
         />
       </div>
