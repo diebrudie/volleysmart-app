4c9a44e5a90b94b5040bc9cd2de01a79fa9ff9ba
Isabel Bruda <ibrudap@gmail.com>
2025-08-04 10:18:55 +0200

    fix: correct set ordering on mobile Dashboard layout Use CSS Grid order classes to fix mobile sequence while preserving desktop layout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -623,7 +623,7 @@ const Dashboard = () => {
           {/* Sets Layout - Grid arrangement */}
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {/* Set 1 - Larger box on the left spanning two rows */}
-            <div className="md:row-span-2">
+            <div className="md:row-span-2 order-1">
               <SetBox
                 key={1}
                 setNumber={1}
@@ -638,8 +638,8 @@ const Dashboard = () => {
               />
             </div>
 
-            {/* Column 2 top row: Set 2 */}
-            <div>
+            {/* Set 2 */}
+            <div className="order-2">
               <SetBox
                 key={2}
                 setNumber={2}
@@ -653,23 +653,8 @@ const Dashboard = () => {
               />
             </div>
 
-            {/* Column 3 top row: Set 4 */}
-            <div>
-              <SetBox
-                key={4}
-                setNumber={4}
-                teamAScore={
-                  scores.find((score) => score.gameNumber === 4)?.teamA
-                }
-                teamBScore={
-                  scores.find((score) => score.gameNumber === 4)?.teamB
-                }
-                onScoreUpdate={handleSetScoreUpdate}
-              />
-            </div>
-
-            {/* Column 2 bottom row: Set 3 */}
-            <div>
+            {/* Set 3 */}
+            <div className="order-3 md:order-4">
               <SetBox
                 key={3}
                 setNumber={3}
@@ -683,8 +668,23 @@ const Dashboard = () => {
               />
             </div>
 
-            {/* Column 3 bottom row: Set 5 */}
-            <div>
+            {/* Set 4 */}
+            <div className="order-4 md:order-3">
+              <SetBox
+                key={4}
+                setNumber={4}
+                teamAScore={
+                  scores.find((score) => score.gameNumber === 4)?.teamA
+                }
+                teamBScore={
+                  scores.find((score) => score.gameNumber === 4)?.teamB
+                }
+                onScoreUpdate={handleSetScoreUpdate}
+              />
+            </div>
+
+            {/* Set 5 */}
+            <div className="order-5 md:order-4">
               <SetBox
                 key={5}
                 setNumber={5}
